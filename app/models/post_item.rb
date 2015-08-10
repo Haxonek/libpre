84abf559e1b8cc5bb59d8ce9e89bc22ba8fe6af3
Haxonek <thomash1996@live.com>
2015-08-10 13:28:56 -0500

    Changed more validations trying to get heroku up

--- app/models/post_item.rb
+++ app/models/post_item.rb
@@ -4,8 +4,10 @@ class PostItem < ActiveRecord::Base
   validates :image_title, length: {maximum: 255}
   validates :description, length: {maximum: 65534}
 
-  has_attached_file :image#, # Maybe comment out compression to see if that's the problem?
+  has_attached_file :image, # Maybe comment out compression to see if that's the problem?
+          :storage => :s3
                     # processors: [:thumbnail, :compression]
-  validates_attachment :image, :content_type => { :content_type => ["image/jpeg", "image/png"] }
+  validates_attachment :image,
+          :content_type => { :content_type => ["image/jpeg", "image/png"] }
   # validates_with AttachmentSizeValidator, :attributes => :image, :less_than => 8.megabytes
 end
