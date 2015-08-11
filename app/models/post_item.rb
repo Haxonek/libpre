eec1137eba29110e2cca943c783c05eeee8c2d37
Haxonek <thomash1996@live.com>
2015-08-11 15:49:57 -0500

    heroku back to compression :/

--- app/models/post_item.rb
+++ app/models/post_item.rb
@@ -4,8 +4,14 @@ class PostItem < ActiveRecord::Base
   validates :image_title, length: {maximum: 255}
   validates :description, length: {maximum: 65534}
 
-  has_attached_file :image#, # Maybe comment out compression to see if that's the problem?
-                    # processors: [:thumbnail, :compression]
-  validates_attachment :image, :content_type => { :content_type => ["image/jpeg", "image/png"] }
-  validates_with AttachmentSizeValidator, :attributes => :image, :less_than => 4.megabytes
+  has_attached_file :image, # Maybe comment out compression to see if that's the problem?
+                    :styles => {thumbnail: "300x300#"},
+                    processors: [:thumbnail, :compression]
+
+  validates :image,
+            attachment_content_type: { content_type: /\Aimage\/.*\Z/ },
+            attachment_size: { less_than: 8.megabytes }
+
+  # validates_attachment :image, :content_type => { :content_type => ["image/jpeg", "image/png"] }
+  # validates_with AttachmentSizeValidator, :attributes => :image, :less_than => 4.megabytes
 end
